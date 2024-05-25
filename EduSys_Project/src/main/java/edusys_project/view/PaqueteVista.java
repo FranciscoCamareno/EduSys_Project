2122fc940f6772a4486f592bf2b7d0c10db9c0c4
FranciscoCamareno <fcamarenougalde@gmail.com>
2024-05-24 20:17:03 -0600

    initial commit

---
+++ EduSys_Project/src/main/java/edusys_project/view/PaqueteVista.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package edusys_project.view;
+
+/**
+ *
+ * @author fcama
+ */
+public class PaqueteVista {
+
+    //Esta clase no hace nada, es solo para que les
+    //aparezacan los paquetes cuando descarguen el repositorio
+    //despues se borra
+}
