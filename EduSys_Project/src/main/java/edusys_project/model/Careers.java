13db135845a749bb604671ebc229f68279175744
FranciscoCamareno <fcamarenougalde@gmail.com>
2024-06-01 17:53:10 -0600

    add tables from database to model package

---
+++ EduSys_Project/src/main/java/edusys_project/model/Careers.java
@@ -0,0 +1,143 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edusys_project.model;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author fcama
+ */
+@Entity
+@Table(name = "careers")
+@NamedQueries({
+    @NamedQuery(name = "Careers.findAll", query = "SELECT c FROM Careers c"),
+    @NamedQuery(name = "Careers.findByCode", query = "SELECT c FROM Careers c WHERE c.code = :code"),
+    @NamedQuery(name = "Careers.findByName", query = "SELECT c FROM Careers c WHERE c.name = :name")})
+public class Careers implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @Column(name = "code")
+    private String code;
+    @Basic(optional = false)
+    @Lob
+    @Column(name = "prof_profile")
+    private String profProfile;
+    @Basic(optional = false)
+    @Column(name = "name")
+    private String name;
+    @Basic(optional = false)
+    @Lob
+    @Column(name = "working_market")
+    private String workingMarket;
+    @Basic(optional = false)
+    @Lob
+    @Column(name = "description")
+    private String description;
+    @JoinColumn(name = "Study_Plan_idStudy_Plan", referencedColumnName = "idStudy_Plan")
+    @ManyToOne(optional = false)
+    private StudyPlan studyPlanidStudyPlan;
+
+    public Careers() {
+    }
+
+    public Careers(String code) {
+        this.code = code;
+    }
+
+    public Careers(String code, String profProfile, String name, String workingMarket, String description) {
+        this.code = code;
+        this.profProfile = profProfile;
+        this.name = name;
+        this.workingMarket = workingMarket;
+        this.description = description;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getProfProfile() {
+        return profProfile;
+    }
+
+    public void setProfProfile(String profProfile) {
+        this.profProfile = profProfile;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getWorkingMarket() {
+        return workingMarket;
+    }
+
+    public void setWorkingMarket(String workingMarket) {
+        this.workingMarket = workingMarket;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public StudyPlan getStudyPlanidStudyPlan() {
+        return studyPlanidStudyPlan;
+    }
+
+    public void setStudyPlanidStudyPlan(StudyPlan studyPlanidStudyPlan) {
+        this.studyPlanidStudyPlan = studyPlanidStudyPlan;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (code != null ? code.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Careers)) {
+            return false;
+        }
+        Careers other = (Careers) object;
+        if ((this.code == null && other.code != null) || (this.code != null && !this.code.equals(other.code))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "edusys_project.model.Careers[ code=" + code + " ]";
+    }
+    
+}
---
+++ EduSys_Project/src/main/java/edusys_project/model/Users.java
@@ -0,0 +1,222 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edusys_project.model;
+
+import java.io.Serializable;
+import java.util.Collection;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author fcama
+ */
+@Entity
+@Table(name = "users")
+@NamedQueries({
+    @NamedQuery(name = "Users.findAll", query = "SELECT u FROM Users u"),
+    @NamedQuery(name = "Users.findByIdUsers", query = "SELECT u FROM Users u WHERE u.idUsers = :idUsers"),
+    @NamedQuery(name = "Users.findByName", query = "SELECT u FROM Users u WHERE u.name = :name"),
+    @NamedQuery(name = "Users.findByPassword", query = "SELECT u FROM Users u WHERE u.password = :password"),
+    @NamedQuery(name = "Users.findByUserName", query = "SELECT u FROM Users u WHERE u.userName = :userName"),
+    @NamedQuery(name = "Users.findByEmail", query = "SELECT u FROM Users u WHERE u.email = :email"),
+    @NamedQuery(name = "Users.findByPhoneNumber", query = "SELECT u FROM Users u WHERE u.phoneNumber = :phoneNumber"),
+    @NamedQuery(name = "Users.findByLastName", query = "SELECT u FROM Users u WHERE u.lastName = :lastName")})
+public class Users implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @Column(name = "idUsers")
+    private Integer idUsers;
+    @Basic(optional = false)
+    @Column(name = "name")
+    private String name;
+    @Basic(optional = false)
+    @Column(name = "password")
+    private String password;
+    @Basic(optional = false)
+    @Column(name = "user_name")
+    private String userName;
+    @Basic(optional = false)
+    @Column(name = "email")
+    private String email;
+    @Basic(optional = false)
+    @Column(name = "phone_number")
+    private String phoneNumber;
+    @Basic(optional = false)
+    @Column(name = "last_name")
+    private String lastName;
+    @ManyToMany(mappedBy = "usersCollection")
+    private Collection<Activities> activitiesCollection;
+    @ManyToMany(mappedBy = "usersCollection")
+    private Collection<Course> courseCollection;
+    @ManyToMany(mappedBy = "usersCollection")
+    private Collection<Notifications> notificationsCollection;
+    @ManyToMany(mappedBy = "usersCollection")
+    private Collection<Groups> groupsCollection;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "users")
+    private Collection<Student> studentCollection;
+    @JoinColumn(name = "Profile_idProfile", referencedColumnName = "idProfile")
+    @ManyToOne(optional = false)
+    private Profile profileidProfile;
+
+    public Users() {
+    }
+
+    public Users(Integer idUsers) {
+        this.idUsers = idUsers;
+    }
+
+    public Users(Integer idUsers, String name, String password, String userName, String email, String phoneNumber, String lastName) {
+        this.idUsers = idUsers;
+        this.name = name;
+        this.password = password;
+        this.userName = userName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.lastName = lastName;
+    }
+
+    public Integer getIdUsers() {
+        return idUsers;
+    }
+
+    public void setIdUsers(Integer idUsers) {
+        this.idUsers = idUsers;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Collection<Activities> getActivitiesCollection() {
+        return activitiesCollection;
+    }
+
+    public void setActivitiesCollection(Collection<Activities> activitiesCollection) {
+        this.activitiesCollection = activitiesCollection;
+    }
+
+    public Collection<Course> getCourseCollection() {
+        return courseCollection;
+    }
+
+    public void setCourseCollection(Collection<Course> courseCollection) {
+        this.courseCollection = courseCollection;
+    }
+
+    public Collection<Notifications> getNotificationsCollection() {
+        return notificationsCollection;
+    }
+
+    public void setNotificationsCollection(Collection<Notifications> notificationsCollection) {
+        this.notificationsCollection = notificationsCollection;
+    }
+
+    public Collection<Groups> getGroupsCollection() {
+        return groupsCollection;
+    }
+
+    public void setGroupsCollection(Collection<Groups> groupsCollection) {
+        this.groupsCollection = groupsCollection;
+    }
+
+    public Collection<Student> getStudentCollection() {
+        return studentCollection;
+    }
+
+    public void setStudentCollection(Collection<Student> studentCollection) {
+        this.studentCollection = studentCollection;
+    }
+
+    public Profile getProfileidProfile() {
+        return profileidProfile;
+    }
+
+    public void setProfileidProfile(Profile profileidProfile) {
+        this.profileidProfile = profileidProfile;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idUsers != null ? idUsers.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Users)) {
+            return false;
+        }
+        Users other = (Users) object;
+        if ((this.idUsers == null && other.idUsers != null) || (this.idUsers != null && !this.idUsers.equals(other.idUsers))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "edusys_project.model.Users[ idUsers=" + idUsers + " ]";
+    }
+    
+}
