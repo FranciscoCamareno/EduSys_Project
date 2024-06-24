1dd3bc977ee64608f623a9f18a9ede964a24b3eb
FranciscoCamareno <fcamarenougalde@gmail.com>
2024-06-24 07:12:31 -0600

    correction
    faltan 2 tablas

--- EduSys_Project/src/main/java/edusys_project/controller/CoursesController.java
+++ EduSys_Project/src/main/java/edusys_project/controller/CoursesController.java
@@ -35,7 +35,7 @@ public class CoursesController implements ActionListener {
        courseJpaController = new CourseJpaController(emf);
        panelCRUD = frameCoursesManagement.getPanelCRUD();                     
        panelCoursesManagement = frameCoursesManagement.getPanelCoursesManagement();
-       frameCoursesManagement.setVisible(true);
+//       frameCoursesManagement.setVisible(true);
        frameCoursesManagement.listen(this);
        frameCoursesManagement.setLocationRelativeTo(null);
     }
